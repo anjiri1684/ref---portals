56ad101c35997e5fac1c74e2406fc3381b62a228
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-06 12:55:14 +0300

    "Refactor TimerChallange component to use interval for timer, add ResultModal, and update UI"

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(1 - (remainingTime / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You {result}</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime}</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,39 +1,60 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 /* eslint-disable react/prop-types */
 function TimerChallange({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timeStarted, setStartedTime] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining - 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-    }, targetTime * 1000);
-    setStartedTime(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 10);
+    }, 10);
   }
 
   function handleStop() {
+    dialog.current.open();
     clearInterval(timer.current);
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You Lost</p>}
-      <p className="challenge-time">
-        Target time: {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timeStarted ? handleStop : handleStart}>
-          {timeStarted ? "Stop" : "Start"} Challange
-        </button>
-      </p>
-      <p className={timeStarted ? "active" : undefined}>
-        {timeStarted ? "Timer is running" : "Timer inactive"}
-      </p>
-    </section>
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        result="lost"
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
+
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          Target time: {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challange
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Timer is running" : "Timer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
 
